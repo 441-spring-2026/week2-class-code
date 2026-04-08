a5adf9fa492e4f2d182c65c244ad167a6ecc86c7
cheri marie <cherimarie@gmail.com>
2026-04-08 14:18:26 -0700

    Make an npm package that plays a sound

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // Run from the command line with 'node index.js'
+// BUG play-sound package only works on MacOS
+var player = require('play-sound')(opts = {})
 
 function activity(){
   console.log("I'm being called from the activity function")
 }
 
-activity();
+function playSound(){
+player.play('whoosh.mp3', function(err){
+    if (err) throw err
+  })
+}
+
+playSound()
