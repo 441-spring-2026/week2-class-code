1da4d8e1d91aacd3462c43c0668658a409fa0545
cheri marie <cherimarie@gmail.com>
2026-04-08 16:02:31 -0700

    Send back data based on route params

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const salmonHarvests ={
+  "2017": {coho: 501392, chinook: 403115},
+  "2018": {coho: 340573, chinook: 387199},
+  "2019": {coho: 312405, chinook:  315566}
+}
+
 // Run with 'node app.js'
 // See output with curl (in new terminal tab) or browser at http://localhost:3000/
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/salmon/:year', (req, res) => {
+  res.send(salmonHarvests[req.params.year])
 })
 
 app.listen(port, () => {
